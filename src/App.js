219ec29fd8ef50cc505fb46217d60c405c62564b
Hernan-Diaz <diazh@live.com.ar>
2022-09-02 22:30:58 -0300

    Entregable FE3 Hernan Diaz

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Cabecera from './components/Cabecera';
+import Listado from './components/Listado';
+import {useState} from 'react';
+
+function App() {
+  const [cantidad, setCantidad] = useState(0);
+
+  function addItem(){
+    setCantidad((cant)=> cant + 1);
+    console.log("Se agrego un item al carrito.");
+  }
+
+  return (
+    <div className="App">
+      <Cabecera cantidad={cantidad}/>
+      <Listado addItem={addItem}/>
+    </div>
+  );
+}
+
+export default App;
